2a96cb3c4ba67e02eccd6f425d54cf2196390a33
shenjoy <3223019380@qq.com>
2025-01-22 23:29:18 +0800

    first change

---
+++ add.py
@@ -0,0 +1,9 @@
+def add(*args):
+    result = 0.0
+    for item in args:
+        if not isinstance(item, (int, float)):
+            raise TypeError('add() only accepts numbers')
+        result += item
+    return result
+
+print(add(1,2,3.1))
