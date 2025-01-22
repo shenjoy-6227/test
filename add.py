14770e35dc8903d8531e86f26ad36010f3c068da
shenjoy <3223019380@qq.com>
2025-01-22 23:42:03 +0800

    forth  change

--- add.py
+++ add.py
@@ -5,5 +5,12 @@ def add(*args):
             raise TypeError('add() only accepts numbers')
         result += item
     return result
-
+def multiply(*args):
+    result = 1.0
+    for item in args:
+        if not isinstance(item, (int, float)):
+            raise TypeError('multiply() only accepts numbers')
+        result *= item
+    return result
 print(add(1,2,3.1))
+print(multiply(1,2,3.1))
